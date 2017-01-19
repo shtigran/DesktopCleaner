a0f1545fb196079d086de4200fe6c08d08ca7144
shtigran <shakhbekyantigran@gmail.com>
2017-01-20 01:07:32 +0400

    Desktop Cleaner

---
+++ Program/Program/CleanClass.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DesktopCleaner
+{
+    public static class CreateTree
+    {
+        static string pathFile;
+
+        public static void Scan(this string path, string punct)
+        {
+            DirectoryInfo direct = new DirectoryInfo(path);
+            DirectoryInfo[] listOfDir = direct.GetDirectories();
+
+            foreach (DirectoryInfo dd in listOfDir)
+            {
+
+                if (dd.Attributes == FileAttributes.Directory)
+                {
+                    if (dd == listOfDir.Last())
+                    {
+
+                        pathFile = path + "\\" + dd;
+                        DirectoryInfo director = new DirectoryInfo(pathFile);
+                        DirectoryInfo[] array = director.GetDirectories();
+                        foreach (DirectoryInfo dir in array)
+                        {
+                            TimeSpan dif = DateTime.Now.Date - dir.CreationTime.Date;
+                            if (dif.TotalDays <= 1)
+                                dir.Delete(true);
+                        }
+
+
+
+                        try
+                        {
+                            pathFile = path + "\\" + dd;
+                            DirectoryInfo di = new DirectoryInfo(pathFile);
+                            FileInfo[] arr = di.GetFiles();
+                            foreach (FileInfo file in arr)
+                            {
+                                TimeSpan dif = DateTime.Now.Date - file.CreationTime.Date;
+                                if (dif.TotalDays <= 1)
+                                    file.Delete();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("The process failed: {0}", e.ToString());
+                        }
+
+                    }
+                    else
+                    {
+                        pathFile = path + "\\" + dd;
+                        DirectoryInfo director = new DirectoryInfo(pathFile);
+                        DirectoryInfo[] array = director.GetDirectories();
+                        foreach (DirectoryInfo dir in array)
+                        {
+                            TimeSpan dif = DateTime.Now.Date - dir.CreationTime.Date;
+                            if (dif.TotalDays <= 1)
+                                dir.Delete(true);
+                        }
+
+                        try
+                        {
+                            pathFile = path + "\\" + dd;
+                            DirectoryInfo di = new DirectoryInfo(pathFile);
+                            FileInfo[] arr = di.GetFiles();
+                            foreach (FileInfo file in arr)
+                            {
+                                TimeSpan dif = DateTime.Now.Date - file.CreationTime.Date;
+                                if (dif.TotalDays <= 1)
+                                    file.Delete();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("The process failed: {0}", e.ToString());
+                        }
+                    }
+
+                    try
+                    {
+                        DirectoryInfo dir = new DirectoryInfo(path + "\\" + dd);
+                        DirectoryInfo[] listOfDirs = dir.GetDirectories();
+                        if (listOfDirs.Length > 0)
+                        {
+
+                            if (dd != listOfDir.Last())
+                            {
+                                Scan(path + "\\" + dd, punct + "  ");
+                            }
+                            else
+                            {
+                                Scan(path + "\\" + dd, punct + "  ");
+                            }
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using DesktopCleaner;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t\t\tCleaner Master\n");
+            Console.WriteLine("Delete temporary folders and files (which have created in last day) \non Your Desktop ");
+
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            path.Scan("");
+
+            Console.WriteLine("\n\nWork is done!");
+
+            DirectoryInfo director = new DirectoryInfo(path);
+            DirectoryInfo[] array = director.GetDirectories();
+            foreach (DirectoryInfo dir in array)
+            {
+                TimeSpan dif = DateTime.Now.Date - dir.CreationTime.Date;
+                if (dif.TotalDays <= 1)
+                    dir.Delete(true);
+            }
+
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] arr = di.GetFiles();
+            foreach (FileInfo file in arr)
+            {
+                TimeSpan dif = DateTime.Now.Date - file.CreationTime.Date;
+                if (dif.TotalDays <= 1)
+                    file.Delete();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
